f7fa0104ad6d4c0780aedc240ce004da89cfa54f
Zeynep Kurı <113287020+ZeynepKURI@users.noreply.github.com>
2024-10-02 18:40:14 +0300

    18.40

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -1,11 +1,18 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace DataAccess.Concrete.EntityFramework
 {
-	public class NorthwindContext
+    //Context:Db tabloları ile proje classlarını bağlamak.
+
+    public class NorthwindContext:DbContext
 	{
-		public NorthwindContext()
-		{
-		}
-	}
+       
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=175.45.2.12");
+        }
+
+    }
 }
 
